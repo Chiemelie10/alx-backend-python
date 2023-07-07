ab73b77096873c40ef65f1f363c12dc9f2268bcb
Chiemelie10 <chiemelieeze@yahoo.com>
2023-07-07 02:22:58 +0000

    Task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines the function to_kv."""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    This function accepts a string and an int or float as its
+    two arguments and returns a Tuple with first and second
+    elements as string and float respectively.
+    """
+
+    return (k, v ** 2)
