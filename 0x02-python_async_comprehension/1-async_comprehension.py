0768e35b7dee1d65f81aebbaff111b91fd967fd5
Chiemelie10 <chiemelieeze@yahoo.com>
2023-07-11 14:51:22 +0000

    Task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module defines a couroutine called async_comprehension."""
+
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    This function returns a list of random numbers generated from
+    'async_generator'.
+    """
+
+    my_numbers = [i async for i in async_generator()]
+
+    return my_numbers
